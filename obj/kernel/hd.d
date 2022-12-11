7c8de01c694864209cca5f2fa0cbdb015a4d1d0a
Tastror <yccozy2001@163.com>
2022-12-12 03:34:59 +0800

    remove obj

--- obj/kernel/hd.d
+++
@@ -1,7 +0,0 @@
-obj/kernel/hd.o: kernel/hd.c /usr/include/stdc-predef.h include/hd.h \
- include/type.h include/proc.h include/protect.h include/fs_const.h \
- include/assert.h include/const.h include/fs.h include/fs_const.h \
- include/fs_misc.h include/fat32.h include/global.h include/const.h \
- include/hd.h include/console.h include/tty.h include/proc.h \
- include/protect.h include/proto.h include/stdio.h include/stdarg.h \
- include/string.h include/type.h include/x86.h
