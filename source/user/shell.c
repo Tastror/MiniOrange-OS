47f70771f3d6204b2adccb2c3d359191eb050f36
Tastror <yccozy2001@163.com>
2023-01-14 14:44:20 +0800

    update shell

--- source/user/shell.c
+++ source/user/shell.c
@@ -1,8 +1,8 @@
 #include <lib/stdio.h>
 #include <lib/string.h>
 
-#include <kernel/syscall.h>
 #include <device/pci.h>
+#include <kernel/syscall.h>
 
 int main(int arg, char *argv[])
 {
@@ -14,27 +14,38 @@ int main(int arg, char *argv[])
     int  pid;
     int  times = 0;
     while (true) {
-        set_color(GREEN);
+        set_color(DARKGREEN);
         printf("MiniOS");
         set_color(BLUE);
         printf(" $ ");
         set_color(WHITE);
         if (gets(buf) && strlen(buf) != 0) {
             if (strcmp(buf, "help") == 0) {
-                printf("\n");
                 set_color(CYAN);
                 printf("HELP MENU\n");
                 set_color(WHITE);
-                printf("use 'orange/xxx.bin' to execute a user file\n");
-                printf("use 'net' to try the net\n");
-                printf("\n");
-            }
-            else if (strcmp(buf, "net") == 0) {
+                printf("use ");
+                set_color(YELLOW);
+                printf("orange/xxx.bin ");
+                set_color(WHITE);
+                printf("to execute a user file\nuse ");
+                set_color(YELLOW);
+                printf("net ");
+                set_color(WHITE);
+                printf("to try the net\n\n");
+            } else if (strcmp(buf, "net") == 0) {
                 printf("trying net now...\n");
-            }
-            else if (exec(buf) != 0) {
+            } else if (strcmp(buf, "exit") == 0) {
+                printf("exiting now.\n");
+                return 0;
+            } else if (exec(buf) != 0) {
                 set_color(RED);
                 printf("exec failed: file not found!\n");
+                printf("you can try ");
+                set_color(YELLOW);
+                printf("help ");
+                set_color(RED);
+                printf("for help.\n\n");
                 set_color(WHITE);
                 continue;
             }
