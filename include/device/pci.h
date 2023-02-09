58cc63ae650eb5da6ccd6395a50181a167b4ebdd
sekrio <1356464784@qq.com>
2023-02-09 17:50:20 +0800

    merge

--- include/device/pci.h
+++ include/device/pci.h
@@ -13,7 +13,6 @@ enum {
 };
 
 struct pci_func {
-<<<<<<< HEAD
     struct pci_bus *bus;  // Primary bus for bridges
 
     uint32_t dev;   // device
@@ -28,16 +27,6 @@ struct pci_func {
     enum {PCI_NOT_MEMORY_BAR = 0, PCI_MEMORY_BAR = 1} reg_type[6];
 
     uint8_t irq_line;  // read from device by PCI_INTERRUPT_REG
-=======
-    struct pci_bus *bus;          // Primary bus for bridges
-    uint32_t        dev;          // device
-    uint32_t        func;         // function
-    uint32_t        dev_id;       // read from device by PCI_ID_REG
-    uint32_t        dev_class;    // read from device by PCI_CLASS_REG
-    uint32_t        reg_base[6];  // bar base
-    uint32_t        reg_size[6];  // bar size
-    uint8_t         irq_line;     // read from device by PCI_INTERRUPT_REG
->>>>>>> b9610aed26056436767a3a73ad0cf2b9bce2e430
 };
 
 struct pci_bus {
