5b0fd725d8b840b947d8c3115c5fa45d66d602a2
sekrio <1356464784@qq.com>
2023-02-10 00:46:35 +0800

    update: we can arp 10.0.2.2(br0) and 10.0.2.1(tap0), use default via 192.168.3.1 dev wlp4s0 proto dhcp metric 600 10.0.2.0/24 dev br0 proto kernel scope link src 10.0.2.2 linkdown 10.0.2.0/24 dev tap0 proto kernel scope link src 10.0.2.1 linkdown 169.254.0.0/16 dev wlp4s0 scope link metric 1000 192.168.3.0/24 dev wlp4s0 proto kernel scope link src 192.168.3.163 metric 600  to check the result

--- include/device/ip.h
+++ include/device/ip.h
@@ -24,7 +24,8 @@ struct ip_hdr {
     uint32_t dst_addr;       // destination ip address
 };
 
-#define local_ip MAKE_IP_ADDR(192, 168, 76, 15)
+// #define local_ip MAKE_IP_ADDR(192, 168, 76, 15)
+#define local_ip MAKE_IP_ADDR(10, 0, 2, 15)
 
 void ip_rx(struct mbuf *m);
 void ip_tx(struct mbuf *m, uint8_t protocol, uint32_t ip);
--- setup_bridge.sh
+++ setup_bridge.sh
@@ -7,17 +7,18 @@ sudo iptables -t filter -A FORWARD -i br0 -j ACCEPT
 sudo iptables -t filter -A FORWARD -o br0 -j ACCEPT
 # 创建tap0设备，让当前用户可以使用
 sudo tunctl -t tap0 -u `whoami`
-# # 清空网卡ip地址
-# sudo ip addr flush dev $1
+# 清空网卡ip地址
+sudo ip addr flush dev $1
+sudo ip addr flush dev tap0
 # 将网卡加入网桥 
-sudo brctl addif br0 $1
-# 将tap0设备加入网桥 
+sudo brctl addif br0 $1 
 sudo brctl addif br0 tap0
 # 启用三个设备
 sudo ifconfig $1 up
 sudo ifconfig tap0 up
 sudo ifconfig br0 up
-
+# 设置ip地址
 sudo ifconfig br0 10.0.2.2/24
-sudo ifconfig $1 0.0.0.0 promisc
-sudo dhclient br0
+sudo ifconfig $1 0.0.0.0 promisc up
+# optional
+sudo dhclient br0 
